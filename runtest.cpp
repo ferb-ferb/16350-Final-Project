eabbd8bcbc2d406827d960ee61b8a2d3598891a1
MisalBhalala <mbhalala@andrew.cmu.edu>
2026-04-18 09:57:38 -0400

    Update runtest.cpp

---
+++ runtest.cpp
@@ -0,0 +1,171 @@
+/*=================================================================
+ *
+ * runtest.cpp
+ *
+ *=================================================================*/
+
+ /*
+ Form of text file
+ W
+200,200
+C
+100
+N
+3
+S
+25,100 #,# #,#
+G
+#,# #,# #,#
+M
+map
+*/
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+
+#include "planner.h"
+
+int main(int argc, char *argv[])
+{
+    // READ PROBLEM
+    if (argc != 2)
+    {
+        std::cout << "runtest takes exactly one command line argument: the map file" << std::endl;
+        return -1;
+    }
+
+    std::cout << "Reading problem definition from: " << argv[1] << std::endl;
+
+    std::ifstream myfile;
+    myfile.open(argv[1]);
+    if (!myfile.is_open()) {
+        std::cout << "Failed to open the file." << std::endl;
+        return -1;
+    }
+
+    // read map size
+    char letter;
+    std::string line;
+    int x_size, y_size;
+
+    myfile >> letter;
+    if (letter != 'W')
+    {
+        std::cout << "error parsing file" << std::endl;
+        return -1;
+    }
+
+    myfile >> x_size >> letter >> y_size;
+    std:: cout << "map size: " << x_size << letter << y_size << std::endl;
+
+    // read collision threshold
+    int collision_thresh;
+    myfile >> letter;
+    if (letter != 'C')
+    {
+        std::cout << "error parsing file" << std::endl;
+        return -1;
+    }
+
+    myfile >> collision_thresh;
+    std:: cout << "collision threshold: " << collision_thresh << std::endl;
+    
+    //get number of robots
+    int num_robots;
+    myfile >> letter;
+    if (letter != 'N')
+    {
+        std::cout << "error parsing file" << std::endl;
+        return -1;
+    }
+
+    myfile >> num_robots;
+    std:: cout << "Number of robots: " << num_robots << std::endl;
+
+    // read start positions
+    int helperX, helperY;
+    std::vector<Location> starts;
+    myfile >> letter;
+    if (letter != 'S')
+    {
+        std::cout << "error parsing file" << std::endl;
+        return -1;
+    }
+    for (int i = 0; i < num_robots; i++)
+    {
+        myfile >> helperX >> letter >> helperY;
+        starts.push_back(Location(helperX, helperY));
+    }
+
+    //read goal positions
+    std::vector<Location> goals;
+    myfile >> letter;
+    if (letter != 'G')
+    {
+        std::cout << "error parsing file" << std::endl;
+        return -1;
+    }
+    for (int i = 0; i < num_robots; i++)
+    {
+        myfile >> helperX >> letter >> helperY;
+        goals.push_back(Location(helperX, helperY));
+    }
+
+    //make agents
+    std::vector<Agent> agents;
+    for (int i = 0; i < num_robots; i++)
+    {
+        agents.push_back(Agent(i, starts[i], goals[i]));
+    }
+
+    // read map
+    std::vector map;
+    for (size_t i=0; i<x_size; i++)
+    {
+        std::getline(myfile, line);
+        std::stringstream ss(line);
+        for (size_t j=0; j<y_size; j++)
+        {
+            double value;
+            ss >> value;
+
+            map[j*x_size+i] = (int) value;
+            if (j != y_size-1) ss.ignore();
+        }
+    }
+
+    myfile.close();
+    std::cout << "\nRunning planner" << std::endl;
+
+    // CONTROL LOOP
+    std::ofstream output_file("robot_trajectory.txt");
+    if (!output_file.is_open()) {
+        std::cerr << "Failed to open the file." << std::endl;
+        return 1;
+    }
+    std::unordered_map<int, Path> plan = CBSPlanner(map, agents);
+
+    int robotposeX, robotposeY;
+    Path curr_path;
+    for (int i = 0; i< num_robots; i++)
+    {
+        
+        curr_path = plan[i];
+        int curr_step = 0;
+        for (Location curr_loc: curr_path)
+        {
+            robotPoseX = curr_loc.get_X();
+            robotPoseY = curr_loc.getY();
+            output_file << i << "," << curr_step << "," << robotposeX << "," << robotposeY << std::endl;
+            curr_step += 1;
+        }
+        printf("\n");
+    }
+
+    output_file.close();
+    return 0;
+}
